d9d5fdf245465493bd96329205560a06a19e1ec1
Robbie Shane P. Orantoy <rsporantoy@headoffice.corp.bpi.com.ph>
2025-12-04 09:44:30 +0800

    Initial commit for Activity 5

---
+++ Activity5.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.bpi.hello;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class HelloMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		 // activity 5
+		 int sum = 0;
+		 int number = 50;
+		 while(number != 0){
+		 	sum = sum + number;
+		 	number--;
+		 }
+		 System.out.println("Sum = " + sum);
+		 // end of Activity 5 */
+		
+		
+		//input.close();
+	}
+
+}
