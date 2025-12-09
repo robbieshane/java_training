be2dafc1695d0bd57da599aca889f1668dcfa471
Robbie Shane P. Orantoy <rsporantoy@headoffice.corp.bpi.com.ph>
2025-12-10 07:52:39 +0800

    push the correct files in M2_Activity1 branch

---
+++ M2_Act1/M2_Activity1.java
@@ -0,0 +1,13 @@
+package M2.OOP1;
+
+public class M2_Activity1 {
+	
+	public static void main(String[] args) {
+		
+		Tree tree1 = new Tree();
+		tree1.type = "Mahogany";
+		tree1.water();
+		
+	}
+
+}
---
+++ M2_Act1/Tree.java
@@ -0,0 +1,10 @@
+package M2.OOP1;
+
+public class Tree{
+	String type;
+		
+	void water() {
+		System.out.println(this.type + " trees at 3-4yrs is watered only twice month");
+	}
+	
+}
