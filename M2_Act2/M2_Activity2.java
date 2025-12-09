20fe53f37bacf9d28477f7dde1454c2c3baa509f
Robbie Shane P. Orantoy <rsporantoy@headoffice.corp.bpi.com.ph>
2025-12-10 07:59:26 +0800

    push the correct files in M2_Activity2 branch

---
+++ M2_Act2/M2_Activity2.java
@@ -0,0 +1,12 @@
+package M2.OOP1;
+
+
+public class M2_Activity2 {
+		
+	public static void main(String[] args) {
+		Whiteboard wb = new Whiteboard();
+		int sum = wb.addNumbers(5,3);
+		System.out.println("The total is: " + String.valueOf(sum));
+	}
+
+}
---
+++ M2_Act2/Whiteboard.java
@@ -0,0 +1,8 @@
+package M2.OOP1;
+
+public class Whiteboard {
+	int addNumbers(int addend1, int addend2) {
+		int sum = addend1 + addend2;
+		return sum;
+	}
+}
